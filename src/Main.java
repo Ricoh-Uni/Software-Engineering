f182611367f8c70dbcbfe169d33a894bdc44e8cb
Ricoh-Uni <ricoh.handt@ruhr-uni-bochum.de>
2022-10-18 21:44:43 +0200

    Exercise 2

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Date;
+
+public class Main {
+    public class Student {
+        String firstname;
+        String lastname;
+        long studentId;
+        double weight;
+        Date birthday;
+
+
+        Student(String firstname, String lastname, long studentId, double weight, Date birthday) {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.studentId = studentId;
+            this.weight = weight;
+            this.birthday = birthday;
+        }
+
+        public enum SortKey {FIRSTNAME, LASTNAME, STUDENT_ID, WEIGHT, BIRTHDAY}
+    }
+}
